4fb062c72d6e3521b493fd9f6c4de03e626d7a32
luanfv <luanfrancavieira@gmail.com>
2022-03-26 21:05:42 -0300

    feat: add write & read & disconnect

--- classic/App.tsx
+++ classic/App.tsx
@@ -8,6 +8,8 @@ import {
   Alert,
   TouchableOpacity,
   Button,
+  Modal,
+  TextInput,
 } from 'react-native';
 import RNBluetoothClassic, {
   BluetoothDevice,
@@ -18,6 +20,8 @@ const App: React.FC = () => {
   const [deviceConnected, setDeviceConnected] = useState<
     BluetoothDevice | undefined
   >(undefined);
+  const [request, setRequest] = useState('');
+  const [isOpenModal, setIsOpenModal] = useState(false);
 
   const handleGetBondedDevices = useCallback(async () => {
     try {
@@ -31,33 +35,83 @@ const App: React.FC = () => {
     } catch (err) {
       Alert.alert(
         'Error',
-        'Could not find any paired connections with your device',
+        'Could not find any paired connections with your device.',
       );
 
       console.log('handleGetBondedDevices error:', err);
     }
   }, []);
 
-  const handleTogglConnectToDevice = useCallback(
-    async (id: string) => {
+  const handleRequest = useCallback(
+    async (data: string) => {
       try {
         if (deviceConnected) {
-          const idConnected = deviceConnected.id;
+          const isConnected = await deviceConnected.isConnected();
 
-          await deviceConnected.disconnect();
-          setDeviceConnected(undefined);
+          if (isConnected) {
+            const response = await deviceConnected.write(data);
 
-          if (idConnected === id) {
-            return;
+            if (response) {
+              Alert.alert(
+                'Success',
+                'Your request has been sent successfully.',
+              );
+
+              return;
+            }
           }
         }
 
-        const device = await RNBluetoothClassic.connectToDevice(id);
+        Alert.alert('Failure', 'Unable to send your request.');
+      } catch (err) {
+        Alert.alert('Error', 'There was an error trying to send your request.');
+
+        console.log('handleRequest error:', err);
+      }
+    },
+    [deviceConnected],
+  );
+
+  const handleDisconnect = useCallback(async () => {
+    try {
+      if (deviceConnected) {
+        const isSuccess = await deviceConnected.disconnect();
+        setDeviceConnected(undefined);
+
+        if (isSuccess) {
+          Alert.alert('Success', 'Device disconnected');
+
+          return;
+        }
+      }
+
+      Alert.alert('Failure', 'Unable to disconnect device');
+    } catch (err) {
+      console.log('handleDisconnect error:', err);
+
+      Alert.alert('Error', 'Unable to disconnect device');
+    }
+  }, [deviceConnected]);
+
+  const handleConnectToDevice = useCallback(
+    async (id: string) => {
+      try {
+        if (deviceConnected) {
+          await deviceConnected.disconnect();
+          setDeviceConnected(undefined);
+        }
+
+        const device = await RNBluetoothClassic.connectToDevice(id, {
+          connectorType: 'rfcomm',
+          delimiter: '\n',
+          charset: Platform.OS === 'ios' ? 1536 : 'utf-8',
+        });
+
         setDeviceConnected(device);
       } catch (err) {
-        Alert.alert('Error', `Unable to connect to device "${id}"`);
+        Alert.alert('Error', `Unable to connect to device "${id}".`);
 
-        console.log('handleTogglConnectToDevice error:', err);
+        console.log('handleConnectToDevice error:', err);
       }
     },
     [deviceConnected],
@@ -85,6 +139,16 @@ const App: React.FC = () => {
     return () => listener.remove();
   }, []);
 
+  useEffect(() => {
+    if (deviceConnected) {
+      const listener = deviceConnected.onDataReceived((item) => {
+        Alert.alert(`${item.device.name} data received`, item.data);
+      });
+
+      return () => listener.remove();
+    }
+  }, [deviceConnected]);
+
   return (
     <View>
       <Text>React Native Bluetooth Classic</Text>
@@ -97,7 +161,13 @@ const App: React.FC = () => {
         renderItem={({ item }) => {
           return (
             <TouchableOpacity
-              onPress={() => handleTogglConnectToDevice(item.id)}
+              onPress={() => {
+                if (deviceConnected) {
+                  setIsOpenModal(true);
+                } else {
+                  handleConnectToDevice(item.id);
+                }
+              }}
               activeOpacity={0.7}
               style={{
                 padding: 20,
@@ -122,6 +192,83 @@ const App: React.FC = () => {
           );
         }}
       />
+
+      <Modal
+        visible={isOpenModal}
+        style={{ backgroundColor: 'red' }}
+        transparent={true}
+        animationType="fade"
+      >
+        <View
+          style={{
+            flex: 1,
+            backgroundColor: 'rgba(0, 0, 0, 0.7)',
+            padding: 20,
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+        >
+          <View style={{ backgroundColor: '#fff', width: '100%', padding: 20 }}>
+            <Text
+              style={{
+                fontWeight: 'bold',
+                fontSize: 18,
+                marginBottom: 40,
+                color: '#000',
+              }}
+            >
+              {deviceConnected?.name}
+            </Text>
+
+            <TextInput
+              placeholder="Request"
+              value={request}
+              onChangeText={setRequest}
+              style={{
+                borderWidth: 1,
+                borderColor: '#000',
+                marginBottom: 20,
+                padding: 10,
+              }}
+            />
+
+            <Button
+              title="write"
+              onPress={() => {
+                handleRequest(request);
+              }}
+            />
+
+            <View
+              style={{
+                width: '100%',
+                height: 1,
+                backgroundColor: '#000',
+                marginVertical: 20,
+              }}
+            />
+
+            <Button title="disconnect" onPress={handleDisconnect} />
+
+            <View
+              style={{
+                width: '100%',
+                height: 1,
+                backgroundColor: '#000',
+                marginVertical: 20,
+              }}
+            />
+
+            <Button
+              title="Close modal"
+              onPress={() => {
+                setIsOpenModal(false);
+                setRequest('');
+              }}
+            />
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 };
